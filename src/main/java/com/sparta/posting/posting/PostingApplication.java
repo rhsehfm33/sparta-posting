4a5eb8f1f591be2c9a18c953a7234fb639f1a618
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-06 10:48:23 +0900

    Initial commit

---
+++ src/main/java/com/sparta/posting/posting/PostingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.posting.posting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/posting/posting/PostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.posting.posting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
