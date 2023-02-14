14e915df79b314f98f21acb2665fcb3ec68e116e
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-14 22:08:38 +0900

    style: Change GenerationType.AUTO to GenerationType.IDENTITY to all entities

--- src/main/java/com/sparta/posting/entity/Board.java
+++ src/main/java/com/sparta/posting/entity/Board.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Board extends Timestamped {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
--- src/main/java/com/sparta/posting/entity/User.java
+++ src/main/java/com/sparta/posting/entity/User.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 public class User extends Timestamped {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
